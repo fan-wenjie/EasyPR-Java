e6f6886421529d3673a6141e2eaa720fe97859c9
Fan Wen Jie <fanwenjie1991@gmail.com>
2015-03-18 14:05:48 +0800

    Add Resources

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 package org.easypr;
 
 
+import org.easypr.core.PlateLocate;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/org/easypr/test/Test.java
@@ -0,0 +1,10 @@
+package org.easypr.test;
+
+
+/*
+ * Created by fanwenjie
+ * @version 1.1
+ */
+
+public class Test {
+}
